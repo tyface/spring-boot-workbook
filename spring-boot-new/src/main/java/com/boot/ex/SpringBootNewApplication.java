1abcd3cf135fac1b2c0573b1037346a4fbe61f2d
TyFace <zxuz34@gmail.com>
2019-01-23 18:42:49 +0900

    스타트 스프링 부트 시작

---
+++ spring-boot-new/src/main/java/com/boot/ex/SpringBootNewApplication.java
@@ -0,0 +1,14 @@
+package com.boot.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNewApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-new/src/main/java/com/boot/ex/domain/SampleVO.java
@@ -0,0 +1,15 @@
+package com.boot.ex.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SampleVO {
+	
+	private String val1;
+	private String val2;
+	private String val3;
+}
